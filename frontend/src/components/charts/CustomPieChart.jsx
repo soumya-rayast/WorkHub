0b3610e92504d1ae1c93db0cb35fc89b389870d5
Soumya Rayast <soumya.rayast@enropesolutions.com>
2025-05-03 12:47:58 +0530

    Working on create task

---
+++ frontend/src/components/charts/CustomPieChart.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, Legend } from 'recharts';
+import CustomToolTip from './CustomToolTip';
+import CustomLegend from './CustomLegend';
+
+const CustomPieChart = ({ data, colors }) => {
+    return (
+        <ResponsiveContainer>
+            <PieChart>
+                <Pie
+                    data={data}
+                    dataKey='count'
+                    nameKey='status'
+                    cx='50%'
+                    cy='50%'
+                    outerRadius={130}
+                    innerRadius={100}
+                    labelLine={false}
+                >
+                    {data.map((entry, index) => {
+                        <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                    })}
+                </Pie>
+                <Tooltip content={<CustomToolTip />} />
+                <Legend content={<CustomLegend />} />
+            </PieChart>
+        </ResponsiveContainer>
+    )
+}
+
+export default CustomPieChart
--- frontend/src/pages/Admin/CreateTask.tsx
+++ frontend/src/pages/Admin/CreateTask.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import DashboardLayout from '../../components/Layouts/DashboardLayout'
 
 const CreateTask = () => {
+  const 
   return (
-    <div>
-      
-    </div>
+    <DashboardLayout activeMenu={'Create Task'}>
+
+    </DashboardLayout>
   )
 }
 
